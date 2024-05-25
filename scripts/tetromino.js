14ea610749d72bcda600f461b16e04359b5fc38c
Mladjan <mladjan.radic@platinpie.com>
2024-05-25 18:28:55 +0200

    add functionalities to tetromino.js

--- scripts/tetromino.js
+++ scripts/tetromino.js
@@ -7,7 +7,14 @@ class Tetromino {
     // to Do
   }
   delete() {
-    // to Do
+    for(let row = 0; row < this.activeTetromino.length; row++){
+
+      for(let col = 0; col < this.activeTetromino.length; col++){
+        if (this.activeTetromino[row][col]) {
+          drawSquare(this.x + col, this.y + row, "white");
+        }
+      }
+    }
   }
   collision(x, y, piece) {
     // to Do
@@ -17,6 +24,11 @@ class Tetromino {
   }
   moveDown() {
     // to Do
+    if (!this.collision(0, 1, this.activeTetromino)) {
+      this.delete();
+      this.y++;
+      this.draw();
+    }
   }
   moveRight() {
     // to Do
